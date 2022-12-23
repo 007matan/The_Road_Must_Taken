cbb939d7a7257e5d05a37517e40bba4c793a8a9e
007matan <1234matan@gmail.com>
2022-12-23 14:19:44 +0200

    Deatails

---
+++ app/src/main/java/com/mygame/theroadmusttaken/CallBack_StepsProtocol.java
@@ -0,0 +1,13 @@
+package com.mygame.theroadmusttaken;
+
+public interface CallBack_StepsProtocol {
+    void smallRightStep();
+    void bigRightStep();
+    void smallLeftStep();
+    void bigLeftStep();
+
+    void fasterStep();
+    void fastestStep();
+    void lowerStep();
+    void lowestStep();
+}
